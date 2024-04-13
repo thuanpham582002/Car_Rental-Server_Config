be62ad8d05c6618feb06aef68715012b5ed7f654
thuanpt <tienthuan05082002@gmail.com>
2024-04-13 21:49:44 +0700

    Initial commit

---
+++ src/main/kotlin/dev/noroom113/rentcarserverconfig/RentCarServerConfigApplication.kt
@@ -0,0 +1,11 @@
+package dev.noroom113.rentcarserverconfig
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RentCarServerConfigApplication
+
+fun main(args: Array<String>) {
+    runApplication<RentCarServerConfigApplication>(*args)
+}
---
+++ src/test/kotlin/dev/noroom113/rentcarserverconfig/RentCarServerConfigApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.noroom113.rentcarserverconfig
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RentCarServerConfigApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
