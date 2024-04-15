d2ec6b005547cbeec5e8280532785fc6bde85cca
thuanpt <tienthuan05082002@gmail.com>
2024-04-15 11:35:02 +0700

    commit before change directory name

--- src/main/kotlin/dev/noroom113/rentcarserverconfig/RentCarServerConfigApplication.kt
+++ src/main/kotlin/dev/noroom113/rentcarserverconfig/RentCarServerConfigApplication.kt
@@ -2,8 +2,12 @@ package dev.noroom113.rentcarserverconfig
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
 class RentCarServerConfigApplication
 
 fun main(args: Array<String>) {
